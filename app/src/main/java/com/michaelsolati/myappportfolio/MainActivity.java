11e4876b4b75ccfa9347ca7fdc7d9c077f097373
Michael Solati <mkslt04@gmail.com>
2016-03-22 00:52:50 -0400

    Initial Commit

---
+++ app/src/main/java/com/michaelsolati/myappportfolio/MainActivity.java
@@ -0,0 +1,39 @@
+package com.michaelsolati.myappportfolio;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void popularMovies (View view) {
+        Toast.makeText(MainActivity.this, "App Not Available Yet", Toast.LENGTH_SHORT).show();
+    }
+
+    public void footballScores (View view) {
+        Toast.makeText(MainActivity.this, "App Not Available Yet", Toast.LENGTH_SHORT).show();
+    }
+
+    public void library (View view) {
+        Toast.makeText(MainActivity.this, "App Not Available Yet", Toast.LENGTH_SHORT).show();
+    }
+
+    public void buildItBigger (View view) {
+        Toast.makeText(MainActivity.this, "App Not Available Yet", Toast.LENGTH_SHORT).show();
+    }
+
+    public void goUbiquitous (View view) {
+        Toast.makeText(MainActivity.this, "App Not Available Yet", Toast.LENGTH_SHORT).show();
+    }
+
+    public void capstone (View view) {
+        Toast.makeText(MainActivity.this, "App Not Available Yet", Toast.LENGTH_SHORT).show();
+    }
+}
