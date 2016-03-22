3929beeabf0d0127ab45b5ac660a192e445a910c
Michael Solati <mkslt04@gmail.com>
2016-03-22 11:31:38 -0400

    Add Scroll To Main Activity XML

--- app/src/main/java/com/michaelsolati/myappportfolio/MainActivity.java
+++ app/src/main/java/com/michaelsolati/myappportfolio/MainActivity.java
@@ -3,6 +3,7 @@ package com.michaelsolati.myappportfolio;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,27 +14,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void popularMovies (View view) {
-        Toast.makeText(MainActivity.this, "App Not Available Yet", Toast.LENGTH_SHORT).show();
-    }
-
-    public void footballScores (View view) {
-        Toast.makeText(MainActivity.this, "App Not Available Yet", Toast.LENGTH_SHORT).show();
-    }
-
-    public void library (View view) {
-        Toast.makeText(MainActivity.this, "App Not Available Yet", Toast.LENGTH_SHORT).show();
-    }
-
-    public void buildItBigger (View view) {
-        Toast.makeText(MainActivity.this, "App Not Available Yet", Toast.LENGTH_SHORT).show();
-    }
-
-    public void goUbiquitous (View view) {
-        Toast.makeText(MainActivity.this, "App Not Available Yet", Toast.LENGTH_SHORT).show();
-    }
-
-    public void capstone (View view) {
-        Toast.makeText(MainActivity.this, "App Not Available Yet", Toast.LENGTH_SHORT).show();
+    public void buttonClicked(View v) {
+        String buttonText = ((Button) v).getText().toString();
+        Toast.makeText(this, "This button launches the " + buttonText + " app", Toast.LENGTH_LONG).show();
     }
 }
