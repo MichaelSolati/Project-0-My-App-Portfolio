1ecae5794ab31c514dffa2f44d8d64ed3c556496
Michael Solati <mkslt04@gmail.com>
2016-05-02 00:19:21 -0400

    Launches Popular Movies app

--- app/src/main/java/com/michaelsolati/myappportfolio/MainActivity.java
+++ app/src/main/java/com/michaelsolati/myappportfolio/MainActivity.java
@@ -1,5 +1,8 @@
 package com.michaelsolati.myappportfolio;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -16,6 +19,34 @@ public class MainActivity extends AppCompatActivity {
 
     public void buttonClicked(View v) {
         String buttonText = ((Button) v).getText().toString();
-        Toast.makeText(this, "This button launches the " + buttonText + " app", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "This button will launch the " + buttonText + " app", Toast.LENGTH_LONG).show();
     }
+
+    public void launchPopularMovies(View v) {
+        String buttonText = ((Button) v).getText().toString();
+        Toast.makeText(this, "Launching the " + buttonText + " app", Toast.LENGTH_LONG).show();
+        openApp(this, "com.michaelsolati.popularmovies");
+    }
+
+    /** Open another app.
+     * @param context current Context, like Activity, App, or Service
+     * @param packageName the full package name of the app to open
+     * @return true if likely successful, false if unsuccessful
+     */
+    public static boolean openApp(Context context, String packageName) {
+        PackageManager manager = context.getPackageManager();
+        try {
+            Intent i = manager.getLaunchIntentForPackage(packageName);
+            if (i == null) {
+                return false;
+                //throw new PackageManager.NameNotFoundException();
+            }
+            i.addCategory(Intent.CATEGORY_LAUNCHER);
+            context.startActivity(i);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
 }
